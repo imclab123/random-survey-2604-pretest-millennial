f16031ae1c30990ad5080d8aef53c73b48f8a773
imclab123 <imc.lab.nccu@gmail.com>
2026-05-04 22:57:24 +0800

    Update url.js

--- url.js
+++ url.js
@@ -11,13 +11,13 @@ const urls = [
 
 'https://zh.surveymonkey.com/r/8B7HYVW', // Google_A_7
 
-'https://zh.surveymonkey.com/r/X3MB5F3', // Google_B_8
+
 
 
 
 'https://zh.surveymonkey.com/r/XFBV6YZ', // Overview_B_10
 
-'https://zh.surveymonkey.com/r/373ZG7B', // GPT_A_11
+
 
 'https://zh.surveymonkey.com/r/37HK6V6' // GPT_B_12
 	
